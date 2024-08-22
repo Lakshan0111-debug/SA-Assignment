fbb42c0d98a588c390fea4c37d0d5f229fef4ea0
chinthaka Lakshan <diluklakshan01@gmail.com>
2024-08-22 23:20:37 +0530

    methods

--- src/Task2/Beverages.java
+++ src/Task2/Beverages.java
@@ -2,4 +2,21 @@ package Task2;
 
 public abstract class Beverages {
     abstract void  addExtras();
+
+    public void  boilWater(){
+        System.out.println("Boiling water");
+    }
+    public void pourInCup(){
+        System.out.println("Pouring into cup ");
+    }
+
+    public final void finalTemplateMethod(){
+        boilWater();
+        brew();
+        pourInCup();
+        addCondiments();
+        if (wantsExtras){
+            addExtras();
+        }
+    }
 }
