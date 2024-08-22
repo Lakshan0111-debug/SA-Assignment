9ab330dd67ddfd8c6d8e6390dee0017c181f3e8a
chinthaka Lakshan <diluklakshan01@gmail.com>
2024-08-23 00:44:23 +0530

    recurrect

--- src/Task3/Rectangle.java
+++ src/Task3/Rectangle.java
@@ -3,16 +3,12 @@ package Task3;
 public class Rectangle implements Shape{
     @Override
     public void draw() {
-        int width = 7;  // You can adjust the width as needed
-        int height = 4; // You can adjust the height as needed
+        int width = 6;
+        int height = 4;
 
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-                if (i == 0 || i == height - 1 || j == 0 || j == width - 1) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
+                System.out.print("*");
             }
             System.out.println();
         }
--- src/Task3/Square.java
+++ src/Task3/Square.java
@@ -3,15 +3,11 @@ package Task3;
 public class Square implements Shape{
     @Override
     public void draw() {
-        int size = 5; // You can adjust the size as needed
+        int size = 5;
 
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
-                if (i == 0 || i == size - 1 || j == 0 || j == size - 1) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
+                System.out.print("*");
             }
             System.out.println();
         }
